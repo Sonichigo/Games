a56e3fa62b3057c9c22038920b003ca13fa2b48b
Animesh Pathak <53110238+Sonichigo@users.noreply.github.com>
2021-01-22 08:40:00 +0530

    Update Pac-man.py

--- Pac-man/Pac-man.py
+++ Pac-man/Pac-man.py
@@ -124,3 +124,36 @@ def move():
             plan = choice(options)
             course.x = plan.x
             course.y = plan.y
+
+        up()
+        goto(point.x + 10, point.y + 10)
+        dot(20, 'red')
+
+    update()
+
+    for point, course in ghosts:
+        if abs(pacman - point) < 20:
+            return
+
+    ontimer(move, 100)
+
+def change(x, y):
+    "Change pacman aim if valid."
+    if valid(pacman + vector(x, y)):
+        aim.x = x
+        aim.y = y
+
+setup(420, 420, 370, 0)
+hideturtle()
+tracer(False)
+writer.goto(160, 160)
+writer.color('white')
+writer.write(state['score'])
+listen()
+onkey(lambda: change(5, 0), 'Right')
+onkey(lambda: change(-5, 0), 'Left')
+onkey(lambda: change(0, 5), 'Up')
+onkey(lambda: change(0, -5), 'Down')
+world()
+move()
+done()
